cd97f5b102e8096b07b9aba2f85f3338c4936b46
Aman Ashutosh <126400177+amanashutosh6998@users.noreply.github.com>
2025-05-21 01:11:27 +0530

    Create MiroEmbed.tsx

---
+++ src/components/MiroEmbed.tsx
@@ -0,0 +1,14 @@
+const MiroEmbed = () => (
+  <div className="w-full h-[600px] rounded-lg overflow-hidden border border-gray-700 shadow">
+    <iframe
+      src="https://miro.com/app/live-embed/uXjVIzIu7OA=/?autoplay=true"
+      width="100%"
+      height="100%"
+      allowFullScreen
+      frameBorder="0"
+      title="Miro Mind Map"
+    />
+  </div>
+);
+
+export default MiroEmbed;
